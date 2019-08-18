fd5d45ee2fa1e293ed849dcfde046760a928c60b
jmgorton <jgorton23@gmail.com>
2019-08-17 19:00:52 -0500

    initial commit - frameworks

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var datapoints = [];
+var bounds = [];
+
+var k = 3;
+
+function setup() {
+	createCanvas(1440, 840);
+
+	for (var i = 0; i < 20; i++) {
+		datapoints[i] = new datapoints();
+	}
+	
+	return;
+}
+
+function draw() {
+	return;
+}
+
